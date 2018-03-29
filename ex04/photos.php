b3bdfa3bddfab023e42c3807f0fb9afa17abb592
Matthew DEVILLE <mdeville@e2r7p21.42.fr>
2018-03-29 15:03:06 +0200

    add ex05

--- ex04/photos.php
+++ ex04/photos.php
@@ -1,9 +1,32 @@
 #!/usr/bin/php
 <?php
 
-if ($argc != 2 || ($ch = curl_init()) == FALSE)
+if ($argc != 2 || ($ch = curl_init()) == FALSE || filter_var($argv[1], FILTER_VALIDATE_URL) == FALSE)
 	exit();
 
+curl_setopt($ch, CURLOPT_URL, $argv[1]);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($ch, CURLOPT_FRESH_CONNECT, 5);
 
+if (($data = curl_exec($ch)) == false)
+	exit();
+curl_close($ch);
 
+$dom = new DOMDocument();
+@$dom->loadHTML($data);
+$url = parse_url($argv[1]);
+@mkdir($url['host']);
+$images = $dom->getElementsByTagName('img');
+foreach ($images as $image) {
+	$ref = $image->getAttribute('src');
+	if (filter_var($ref, FILTER_VALIDATE_URL) == FALSE) {
+		@copy($argv[1].$ref, $url['host'] . "/" . basename($ref));
+	}
+	else {
+		@copy($ref, $url['host'] . "/" . basename($ref));
+	}
+}
 ?>
---
+++ ex05/denial.php
@@ -0,0 +1,29 @@
+#!/usr/bin/php
+<?php
+
+if ($argc != 3)
+	exit();
+
+if (($fd = fopen($argv[1], 'r')) === FALSE || ($keys = fgetcsv($fd, 0, ";")) === FALSE || !in_array($argv[2], $keys))
+	exit();
+foreach ($keys as $value) {
+	${$value} = array();
+}
+while (($line = fgetcsv($fd, 0, ";")) !== FALSE) {
+	if (count($keys) > count($line))
+		exit();
+	for($i = 0; $i < count($keys); $i++) {
+		$index = array_search($argv[2], $keys);
+		${$keys[$i]}[$line[$index]] = $line[$i];
+	}
+}
+fclose($fd);
+while (true)
+{
+	echo "Enter your command: ";
+	$out = fgets(STDIN);
+	if (feof(STDIN))
+		exit(0);
+	eval($out);
+}
+?>
